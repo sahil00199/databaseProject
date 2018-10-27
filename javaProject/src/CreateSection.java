c5f5b2f4dc9acab6695d44e60f43df2315f8a1be
Aman Bansal <aman005bansal003@gmail.com>
2018-10-27 17:48:21 +0530

    removed buggy line

--- javaProject/src/CreateSection.java
+++ javaProject/src/CreateSection.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.json.*;
+//import org.json.*;
 
 import java.sql.Connection;
 
