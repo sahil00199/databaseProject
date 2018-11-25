b004c367562ac185ac1dbc652c46a6c2f6686cdd
Aman Bansal <aman005bansal003@gmail.com>
2018-11-25 18:24:59 +0530

    done adding ta

--- javaProject/WebContent/instructor_section.js
+++ javaProject/WebContent/instructor_section.js
@@ -4,7 +4,7 @@ function buildList(result, list)
     list.html('');
     if(result != ''){
     	$.each(result, function(k, v) {
-    		console.log(v);
+//    		console.log(v);
             list.append("<h3> <a href = \"InstructorQuiz?qzid=" + v.qzid + "\"> " + v.qzname + " </a></h3>");
         });
     }
@@ -31,7 +31,7 @@ function loadQs(){
         success: function(data){
 //        	console.log(data);
         	var data1 = (jQuery.parseJSON(data));
-        	console.log(data1);
+//        	console.log(data1);
         	if(data1.status){
 	            buildList(
 	                data1.data,
@@ -40,7 +40,7 @@ function loadQs(){
         	}
         	else{
         		window.location.replace("illegalAccess.html");
-        		console.log(data1.message);
+//        		console.log(data1.message);
         	}
         }
     });
@@ -51,6 +51,12 @@ $(document).ready(function() {
     {
         e.preventDefault();
         showCreateQuiz();
+    }  );
+    $("#talinker").click(function(e)
+    {
+//    	console.log("this");
+    	e.preventDefault();
+    	showTA();
     }  );
 });
 
@@ -66,6 +72,43 @@ function showCreateQuiz()
 //    "value=\"Submit\" />"+
     "</form>";
     document.getElementById("newConvo").innerHTML = currentHTML;
+}
+
+function showTA()
+{
+    var currentHTML = "<form>" +
+    "Enter the taid: <input type=\"text\" id = \"taid\" name=\"taid\">"+
+    "<input type=\"text\" id = \"tapid\" name=\"tapid\" hidden>"+
+    "<button type=\"button\" onclick=\"addTAQuiz()\">Submit</button>"+
+//    "value=\"Submit\" />"+
+    "</form>";
+    document.getElementById("tanewConvo").innerHTML = currentHTML;
+    $("#taid").autocomplete({
+        source : function(request,response){
+            var xhttp;
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function(){
+                 if (this.readyState == 4 && this.status == 200){
+                     json= JSON.parse(this.responseText);
+//                     console.log(document.getElementById("tttopic").value);
+//                     console.log(json.data);
+                     for (var i = 0; i < json.data.length; i++){
+                    	 json.data[i].label = json.data[i].value + "_" + json.data[i].label;
+                     }
+//                     console.log(json.data);
+                     response(json.data);
+//                     console.log(document.getElementById("tttopic").value);
+                 }
+            }
+            xhttp.open("GET", "AutoCompleteTa?partial="+request.term, true);
+            xhttp.send();
+        },
+        select: function (event, ui) {
+//        	var toshow = ui.item.label + "_" + ui.item.value;
+            $("#taid").val(ui.item.label); // display the selected text
+            $("#tapid").val(ui.item.value); // save selected id to hidden input
+        }
+    });
 }
 
 function createNewQuiz()
@@ -91,6 +134,31 @@ function createNewQuiz()
          }
     }
     xhttp.open("GET", "CreateQuiz?coursen=" + qzname + "&secid="+ secid +"&starttime="+starttime+"&duration="+duration+"&maxmarks="+maxmarks+"&weightage="+weightage, true);
+    xhttp.send();
+    loadQs();
+}
+
+function addTAQuiz()
+{
+//	console.log("here");
+	var tid = document.getElementById('tapid').value;
+//	console.log(tid);
+	var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+         if (this.readyState == 4 && this.status == 200){
+//        	 console.log(this.responseText);
+		    json_object = JSON.parse(this.responseText);
+		    if(!(json_object.status)){
+		        alert(json_object.message);
+		    }
+		    else{
+		        alert("TA added successfully!");
+		    }
+		    loadQs();
+         }
+    }
+    xhttp.open("GET", "AddTaToSection?secid="+ secid +"&taid="+tid, true);
     xhttp.send();
     loadQs();
 }
